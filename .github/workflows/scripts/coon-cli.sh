1b9f12be5279ca746cfebd6c67d13d0cc9b4b7c1
leenchan <leenchan@gmail.com>
2024-05-13 17:22:06 +0800

    update

--- .github/workflows/scripts/coon-cli.sh
+++ .github/workflows/scripts/coon-cli.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+CUR_DIR=$(cd "$(dirname "$0" 2>/dev/null)";pwd)
 
 coon_cli_install() {
-	[ -z "$COON_CLI_URL" ] && return 1
-	sudo curl -skL -o /usr/bin/coon-cli "$COON_CLI_URL" && \
-		sudo chmod +x /usr/bin/coon-cli && \
-		echo "COON_CLI_BIN=/usr/bin/coon-cli" >> $GITHUB_ENV
+	# [ -z "$COON_CLI_URL" ] && return 1
+	# sudo curl -skL -o /usr/bin/coon-cli "$COON_CLI_URL" && \
+	# 	sudo chmod +x /usr/bin/coon-cli && \
+	# 	echo "COON_CLI_BIN=/usr/bin/coon-cli" >> $GITHUB_ENV
+	sudo tar -xf "$CUR_DIR/bin.tar.gz" -C /usr/bin && sudo chmod +x /usr/bin/coon-cli && echo "COON_CLI_BIN=/usr/bin/coon-cli" >> $GITHUB_ENV
 }
 
 coon_cli_install
